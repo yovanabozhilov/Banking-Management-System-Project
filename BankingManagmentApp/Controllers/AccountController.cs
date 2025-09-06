ab20bb8bb0d61b924b2a92da05f669c0a3a1f7d4
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-09-06 12:40:20 +0300

    Switching to SixLabors.ImageSharp

--- BankingManagmentApp/Controllers/AccountController.cs
+++ BankingManagmentApp/Controllers/AccountController.cs
@@ -1,25 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
-public class AccountController : Controller 
+namespace BankingManagmentApp.Controllers
 {
-    public IActionResult CaptchaImage()
+    public class AccountController : Controller
     {
-        // Генерираме случайна 4-цифрена CAPTCHA
-        var captchaText = new Random().Next(1000, 9999).ToString();
-        HttpContext.Session.SetString("Captcha", captchaText);
+        public IActionResult CaptchaImage()
+        {
+            var captchaText = new Random().Next(1000, 9999).ToString();
+            HttpContext.Session.SetString("Captcha", captchaText);
 
-        using var bmp = new Bitmap(120, 40);
-        using var gfx = Graphics.FromImage(bmp);
+            using var image = new Image<Rgba32>(120, 40, Color.White);
 
-        gfx.Clear(Color.White);
-        using var font = new Font("Arial", 20, FontStyle.Bold);
-        gfx.DrawString(captchaText, font, Brushes.Black, new PointF(10, 5));
+            var font = SystemFonts.CreateFont("Arial", 20, FontStyle.Bold);
+            image.Mutate(ctx => ctx.DrawText(captchaText, font, Color.Black, new PointF(10, 5)));
 
-        using var ms = new MemoryStream();
-        bmp.Save(ms, ImageFormat.Png);
-        return File(ms.ToArray(), "image/png");
+            using var ms = new MemoryStream();
+            image.Save(ms, new PngEncoder());
+
+            return File(ms.ToArray(), "image/png");
+        }
     }
 }
