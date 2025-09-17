4c661a6a37ed6dc3476b218f7013303dec8d98ef
Yovana <jovanabozilov02@yahoo.com>
2025-09-18 00:25:25 +0300

    EF Core migration: PersonalQnAWithChatTools

---
+++ BankingManagmentApp/Migrations/20250917212452_PersonalQnAWithChatTools.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingManagmentApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersonalQnAWithChatTools : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Keyword",
+                table: "TemplateAnswer",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FunctionName",
+                table: "TemplateAnswer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemplateAnswer_Keyword",
+                table: "TemplateAnswer",
+                column: "Keyword");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TemplateAnswer_Keyword",
+                table: "TemplateAnswer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Keyword",
+                table: "TemplateAnswer",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FunctionName",
+                table: "TemplateAnswer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
