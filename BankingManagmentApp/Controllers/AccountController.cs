d562198c854df9f52233c36c1f7360930b5abf02
Yovana <jovanabozilov02@yahoo.com>
2025-09-18 18:22:16 +0300

    Initial commit: Banking Management System

--- BankingManagmentApp/Controllers/AccountController.cs
+++ BankingManagmentApp/Controllers/AccountController.cs
@@ -1,29 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Drawing;              
+using SixLabors.ImageSharp.Drawing.Processing;   
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
-namespace BankingManagmentApp.Controllers
+public class AccountController : Controller
 {
-    public class AccountController : Controller
+    [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+    public IActionResult CaptchaImage()
     {
-        public IActionResult CaptchaImage()
+        var captchaText = RandomDigits(4);
+        HttpContext.Session.SetString("Captcha", captchaText);
+
+        var fontPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "fonts", "DejaVuSans.ttf");
+        var collection = new FontCollection();
+        var family = collection.Add(fontPath);
+        var font = family.CreateFont(24, FontStyle.Bold);
+
+        using var img = new Image<Rgba32>(120, 40);
+        img.Mutate(ctx =>
         {
-            var captchaText = new Random().Next(1000, 9999).ToString();
-            HttpContext.Session.SetString("Captcha", captchaText);
+            ctx.Fill(Color.White);
 
-            using var image = new Image<Rgba32>(120, 40, Color.White);
+            var pb = new PathBuilder();
+            pb.AddLine(new PointF(0, 10),  new PointF(120, 15));
+            pb.AddLine(new PointF(0, 25),  new PointF(120, 30));
+            var path = pb.Build();
+            ctx.Draw(Color.LightGray, 1, path);
 
-            var font = SystemFonts.CreateFont("Arial", 20, FontStyle.Bold);
-            image.Mutate(ctx => ctx.DrawText(captchaText, font, Color.Black, new PointF(10, 5)));
+            ctx.DrawText(captchaText, font, Color.Black, new PointF(10, 5));
+        });
 
-            using var ms = new MemoryStream();
-            image.Save(ms, new PngEncoder());
-
-            return File(ms.ToArray(), "image/png");
-        }
+        using var ms = new MemoryStream();
+        img.Save(ms, new PngEncoder());
+        return File(ms.ToArray(), "image/png");
     }
+
+    [HttpPost]
+    public IActionResult VerifyCaptcha(string captchaInput)
+    {
+        var expected = HttpContext.Session.GetString("Captcha");
+        if (!string.IsNullOrEmpty(expected) &&
+            string.Equals(expected, captchaInput, StringComparison.Ordinal))
+        {
+            return Ok();
+        }
+        return BadRequest("CAPTCHA mismatch");
+    }
+
+    private static string RandomDigits(int n) =>
+        string.Concat(Enumerable.Range(0, n).Select(_ => Random.Shared.Next(0, 10)));
 }
